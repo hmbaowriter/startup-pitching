7c26a728bef6733ff6e4297a111a5cca09d02db3
Tommy Evans <webdesign.minhbao@gmail.com>
2025-03-01 14:59:22 +0700

    add search query params

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -9,11 +9,12 @@ export default async function HomePage({
   searchParams: Promise<{ query?: string }>;
 }) {
   const query = (await searchParams).query;
+  const params = { search: query || null };
 
   // Go to https://www.sanity.io/learn/course/content-driven-web-application-foundations/fetch-sanity-content to read more about Sanity data fetching
   // const posts = await client.fetch(STARTUPS_QUERY);
 
-  const { data: posts } = await sanityFetch({ query: STARTUPS_QUERY });
+  const { data: posts } = await sanityFetch({ query: STARTUPS_QUERY, params });
 
   return (
     <>
--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -1,14 +1,22 @@
 import { defineQuery } from "next-sanity";
 
+// https://www.sanity.io/docs/live-content-guide for more information
 export const STARTUPS_QUERY =
-  defineQuery(`*[_type=="startup" && defined(slug.current)] | order(_createdAt desc) {
-  _id, 
-  title,
-  slug, 
-  _createdAt, 
-  author -> { _id, name, image, bio },
-  views,
-  description, 
-  category, 
-  image
-}`); 
+  defineQuery(`
+  *[_type=="startup" && defined(slug.current) && !defined($search) 
+    || title match $search 
+    || category match $search 
+    || author->name match $search 
+    | order(_createdAt desc)
+  ] {
+    _id, 
+    title,
+    slug, 
+    _createdAt, 
+    author -> { _id, name, image, bio },
+    views,
+    description, 
+    category, 
+    image
+  }
+`); 
